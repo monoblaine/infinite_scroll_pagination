21b2ed4396866477c8193eb27b384d768c76c808
Serhan Apaydın <zerhan@gmail.com>
2021-11-17 09:57:43 +0300

    Remove padding from FirstPageProgressIndicator

--- lib/src/ui/default_indicators/first_page_progress_indicator.dart
+++ lib/src/ui/default_indicators/first_page_progress_indicator.dart
@@ -2,10 +2,5 @@ import 'package:flutter/material.dart';
 
 class FirstPageProgressIndicator extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => const Padding(
-        padding: EdgeInsets.all(32),
-        child: Center(
-          child: LinearProgressIndicator(),
-        ),
-      );
+  Widget build(BuildContext context) => const LinearProgressIndicator();
 }
